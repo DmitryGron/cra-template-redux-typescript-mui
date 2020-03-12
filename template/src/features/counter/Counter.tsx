4796a824603d1af3cc64ab8c3a1b157eaaa1404d
Jake Baker <jacob@xors.com>
2020-03-12 15:14:38 +0000

    Port Counter CSS to material-ui

--- template/src/features/counter/Counter.tsx
+++ template/src/features/counter/Counter.tsx
@@ -1,60 +1,145 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import {
   decrement,
   increment,
   incrementByAmount,
   incrementAsync,
-  selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+  selectCount
+} from "./counterSlice";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Input from "@material-ui/core/Input";
+import Typography from "@material-ui/core/Typography";
+import stylesold from "./Counter.module.css";
+import {
+  withStyles,
+  Theme,
+  StyleRules,
+  createStyles,
+  WithStyles
+} from "@material-ui/core";
 
-export function Counter() {
+const styles: (theme: Theme) => StyleRules<string> = theme =>
+  createStyles({
+    value: {
+      fontSize: "78px"
+    },
+    button: {
+      color: "rgb(112, 76, 182)",
+      appearance: "none",
+      background: "none",
+      fontSize: "32px",
+      paddingLeft: "12px",
+      paddingRight: "12px",
+      paddingBottom: "4px",
+      cursor: "pointer",
+      backgroundColor: "rgba(112, 76, 182, 0.1)",
+      borderRadius: "2px",
+      transition: "all 0.15s",
+      outline: "none",
+      border: "2px solid transparent",
+      "&:hover": {
+        backgroundColor: "#fff",
+        border: "2px solid rgba(112, 76, 182, 0.4)"
+      },
+      "&:focus": {
+        border: "2px solid rgba(112, 76, 182, 0.4)"
+      },
+      "&:active": {
+        backgroundColor: "rgba(112, 76, 182, 0.2)"
+      }
+    },
+    textbox: {
+      fontSize: "32px",
+      width: "64px",
+      textAlign: "center"
+    },
+    asyncButton: {
+      "&:after": {
+        content: "",
+        backgroundColor: "rgba(112, 76, 182, 0.15)",
+        display: "block",
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        left: "0",
+        top: "0",
+        opacity: "0",
+        transition: "width 1s linear, opacity 0.5s ease 1s",
+      },
+      "&:active:after": {
+        width: "0%",
+        opacity: "1",
+        transition: "0s"
+      }
+    }
+  });
+
+type CounterProps = {} & WithStyles<typeof styles>;
+
+const Counter = ({ classes }: CounterProps) => {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  const [incrementAmount, setIncrementAmount] = useState("2");
 
   return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
+    <React.Fragment>
+    <Grid container xs={12} alignItems="center" justify="center" spacing={3}>
+      <Grid item>
+        <Button
+          className={classes.button}
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
         >
           +
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
+        </Button>
+      </Grid>
+      <Grid item>
+        <Typography className={classes.value} variant="body1">
+          {count}
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Button
+          className={classes.button}
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
           -
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
+        </Button>
+      </Grid>
+      </Grid>
+      <Grid container xs={12} alignItems="center" justify="center" spacing={3}>
+        <Grid item>
+        <Input
+          className={classes.textbox}
           aria-label="Set increment amount"
           value={incrementAmount}
           onChange={e => setIncrementAmount(e.target.value)}
         />
-        <button
-          className={styles.button}
+        </Grid>
+        <Grid item>
+        <Button
+          className={classes.button}
           onClick={() =>
             dispatch(incrementByAmount(Number(incrementAmount) || 0))
           }
         >
           Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
+        </Button>
+        </Grid>
+        <Grid item>
+        <Button
+          className={[classes.button, classes.asyncButton].join(" ")}
           onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}
         >
           Add Async
-        </button>
-      </div>
-    </div>
+        </Button>
+        </Grid>
+      </Grid>
+    </React.Fragment>
   );
-}
+};
+
+export default withStyles(styles)(Counter);
